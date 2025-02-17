f3598633b6bc4e4cab967e4a3f5fe7200ee003af
elazizmed <elazizmohammedamine@gmail.com>
2025-02-17 16:31:29 +0100

    create a query for charges & passive for can see all money movments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # restaurant_db
 analyse database of a restaurant
+if you need to test this script :
+export database in your localhost .. the folder of database is excel_data
--- anaylise_rest.py
+++ anaylise_rest.py
@@ -21,28 +21,48 @@ query1 = """SELECT o.item_id , m.item_name, m.category,
 m.price, count(*) as all_sales , 
 CONCAT('$', FORMAT(m.price * COUNT(*), 2)) AS income 
 FROM menu_items m INNER JOIN order_details o ON m.menu_item_id = o.item_id 
-GROUP BY o.item_id ORDER BY `income` DESC"""
+GROUP BY o.item_id ORDER BY m.price * COUNT(*) DESC"""
 
 
 query2 = """SELECT m.category, count(*) as all_sales , 
 CONCAT('$', FORMAT(m.price * COUNT(*), 2)) AS income 
 FROM menu_items m INNER JOIN order_details o ON m.menu_item_id = o.item_id 
-GROUP BY m.category ORDER BY `income` DESC"""
+GROUP BY m.category ORDER BY m.price * COUNT(*) DESC"""
+
+query3 = """SELECT 
+    o.item_id, 
+    m.item_name, 
+    m.category, 
+    m.price, 
+    COUNT(*) AS all_sales, 
+    CONCAT('$', FORMAT(m.price * COUNT(*), 2)) AS income, 
+    CONCAT('$', FORMAT(m.price * COUNT(*) * 0.45, 2)) AS charges, 
+    CONCAT('$', FORMAT((m.price * COUNT(*)) - (m.price * COUNT(*) * 0.45), 2)) AS passive_income
+FROM menu_items m 
+INNER JOIN order_details o ON m.menu_item_id = o.item_id 
+GROUP BY o.item_id 
+ORDER BY m.price * COUNT(*) DESC"""
 
 
 cursor.execute(query1)
 data1 = cursor.fetchall()
 df1 = pd.DataFrame(data1, columns=["Order_item_id", "item_name", "category", "price", "number_of_orders_by_item", "income"])
 
+
 cursor.execute(query2)
 data2 = cursor.fetchall()
 df2 = pd.DataFrame(data2, columns=["category", "number_of_orders_by_item", "income"])
+
+cursor.execute(query3)
+data3 = cursor.fetchall()
+df3 = pd.DataFrame(data3, columns=["Order_item_id", "item_name", "category", "price", "number_of_orders_by_item", "income" , "chargers" , "passive"])
 
 
 file_path = os.path.join(folder_path, "orders_and_products.xlsx")
 with pd.ExcelWriter(file_path, engine='xlsxwriter') as writer:
     df1.to_excel(writer, sheet_name="income", index=False)
     df2.to_excel(writer, sheet_name="income_by_category", index=False)
+    df3.to_excel(writer, sheet_name="charges&passive", index=False)
 
 
 cursor.close()
