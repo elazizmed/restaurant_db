03936d2322c72b081f63493bf0a6113a67bec916
elazizmed <elazizmohammedamine@gmail.com>
2025-02-18 16:19:21 +0100

    fetch income from excel and make sum on income for grt total income

--- anaylise_rest.py
+++ anaylise_rest.py
@@ -24,20 +24,16 @@ FROM menu_items m INNER JOIN order_details o ON m.menu_item_id = o.item_id
 GROUP BY o.item_id ORDER BY m.price * COUNT(*) DESC"""
 
 
-query2 = """SELECT m.category, count(*) as all_sales , 
+query2 = """SELECT m.category, count(*) as all_sales, 
 CONCAT('$', FORMAT(m.price * COUNT(*), 2)) AS income 
 FROM menu_items m INNER JOIN order_details o ON m.menu_item_id = o.item_id 
 GROUP BY m.category ORDER BY m.price * COUNT(*) DESC"""
 
-query3 = """SELECT 
-    o.item_id, 
-    m.item_name, 
-    m.category, 
-    m.price, 
-    COUNT(*) AS all_sales, 
-    CONCAT('$', FORMAT(m.price * COUNT(*), 2)) AS income, 
-    CONCAT('$', FORMAT(m.price * COUNT(*) * 0.45, 2)) AS charges, 
-    CONCAT('$', FORMAT((m.price * COUNT(*)) - (m.price * COUNT(*) * 0.45), 2)) AS passive_income
+query3 = """SELECT o.item_id, m.item_name, m.category, m.price, 
+COUNT(*) AS all_sales, 
+CONCAT('$', FORMAT(m.price * COUNT(*), 2)) AS income, 
+CONCAT('$', FORMAT(m.price * COUNT(*) * 0.45, 2)) AS charges, 
+CONCAT('$', FORMAT((m.price * COUNT(*)) - (m.price * COUNT(*) * 0.45), 2)) AS passive_income
 FROM menu_items m 
 INNER JOIN order_details o ON m.menu_item_id = o.item_id 
 GROUP BY o.item_id 
@@ -57,12 +53,21 @@ cursor.execute(query3)
 data3 = cursor.fetchall()
 df3 = pd.DataFrame(data3, columns=["Order_item_id", "item_name", "category", "price", "number_of_orders_by_item", "income" , "chargers" , "passive"])
 
-
 file_path = os.path.join(folder_path, "orders_and_products.xlsx")
+df4 = pd.read_excel(file_path, sheet_name="income")
+
+sum_income_df4 = df4['income'].replace({'\$': '', ',': ''}, regex=True).astype(float).sum()
+summary_data = {
+    "Sheet Name": ["income"],
+    "Total Income": [sum_income_df4]
+}
+
+summary_df = pd.DataFrame(summary_data)
 with pd.ExcelWriter(file_path, engine='xlsxwriter') as writer:
     df1.to_excel(writer, sheet_name="income", index=False)
     df2.to_excel(writer, sheet_name="income_by_category", index=False)
     df3.to_excel(writer, sheet_name="charges&passive", index=False)
+    summary_df.to_excel(writer, sheet_name="test", index=False)
 
 
 cursor.close()
